8b0b13b1bb200cad0173e448b62285ca28867a8a
Italo Teixeira <italogustavoborba@gmail.com>
2023-12-14 11:18:28 -0300

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,11 @@
 import { type Frame, VisionCameraProxy } from 'react-native-vision-camera';
 
-export type FaceDirection =
-  | 'left-skewed'
-  | 'right-skewed'
-  | 'frontal'
-  | 'transitioning'
-  | 'unknown';
-
-/**
- *
- * @ErrorCode :
- * - 101: system error`
- * - 102: plugin not found
- * - 103: cannot get image from frame
- * - 104: faces not found
- * - 105: too many faces in frame
- * - 106: face is out of frame
- * - 107: face is transitioning
- */
-
-type FaceDetectionErrorCode = 101 | 102 | 103 | 104 | 105 | 106 | 107;
-export type FaceDetectionStatus = 'success' | 'standby' | 'error';
-
-export type FaceDetectorResponse = {
-  status: FaceDetectionStatus;
-  faceDirection: FaceDirection;
-  frameData?: string;
-  error?: {
-    code: FaceDetectionErrorCode;
-    message: string;
-  };
-};
-
 const plugin = VisionCameraProxy.initFrameProcessorPlugin('detectFace', {});
 
-export function detectFace(frame: Frame): FaceDetectorResponse {
+export function detectFace(frame: Frame) {
   'worklet';
   if (!plugin) {
     return {
-      status: 'error',
-      faceDirection: 'unknown',
       error: {
         code: 102,
         message: 'Plugin not found',
