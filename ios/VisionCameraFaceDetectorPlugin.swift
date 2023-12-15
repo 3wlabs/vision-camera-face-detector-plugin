77c20845e8adc73eb3bf2ccc4a929c2e64efcb5b
Italo Teixeira <italogustavoborba@gmail.com>
2023-12-15 14:41:20 -0300

    wip

--- ios/VisionCameraFaceDetectorPlugin.swift
+++ ios/VisionCameraFaceDetectorPlugin.swift
@@ -101,7 +101,9 @@ public class VisionCameraFaceDetectorPlugin: FrameProcessorPlugin {
           "width": frameRect.width,
           "height": frameRect.height,
           "boundingCenterX": frameRect.midX,
-          "boundingCenterY": frameRect.midY
+          "boundingCenterY": frameRect.midY,
+          "offsetX": offsetX,
+          "offsetY": offsetY
         ]
     }
     
